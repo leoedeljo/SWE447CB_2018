af753ac24a9f98baf5a03561c5dd5d72c9c8ff05
deoelNoveno <leoedeljo@yahoo.com>
2018-02-14 14:23:32 -0800

    Update Solar.js

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -154,10 +154,14 @@ function render() {
   // planet.PointMode = false;
   
   ms.push();
-  ms.rotate(planet.year, [0, 1, 0]);
-  ms.translate(planet.distance, 0, 0);
-  ms.rotate(planet.day, [0, 1, 0]);
-  ms.scale(planet.radius);
+  ms.rotate(data.year, [0, 1, 0]);
+  ms.translate(data.distance, 0, 0);
+  ms.rotate(data.day, [0, 1, 0]);
+  ms.scale(data.radius);
+  gl.useProgram(planet.program);
+  gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
+  gl.uniformMatrix4fv(planet.uniforms.P, false, flatten(P));
+  gl.uniform4fv(planet.uniforms.color, flatten(data.color));
   planet.render();
   ms.pop();
   
