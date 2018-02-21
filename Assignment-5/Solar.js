be4d34e55a5cb34e090141bc42d9be8beac76059
deoelNoveno <leoedeljo@yahoo.com>
2018-02-20 19:49:29 -0800

    Update Solar.js

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -19,7 +19,7 @@ var gl;
 var Planets = {
   Sun : undefined,
   Mercury : undefined,
-  // Venus : undefined,
+  Venus : undefined,
   Earth : undefined,
   Moon : undefined,
   // Mars : undefined,
@@ -115,7 +115,7 @@ function render() {
   // about the planets in SolarSystem.  Look at how these are
   // used; it'll simplify the work you need to do.
 
-  var name, planet, data, name1, name2, name3, name4;
+  var name, planet, data, name1, name2, name3, name4, name5;
 
   name = "Sun";
   planet = Planets[name];
@@ -203,6 +203,25 @@ function render() {
   name4 = "Mercury";
   planet = Planets[name4];
   data = SolarSystem[name4];
+  
+  planet.PointMode = false;
+  
+  ms.push();
+  ms.rotate((1/data.year)*time, [0, 0, 1]);
+  ms.translate(data.distance, 0, 0);
+  ms.scale(data.radius);
+  gl.useProgram(planet.program);
+  gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
+  gl.uniformMatrix4fv(planet.uniforms.P, false, flatten(P));
+  gl.uniform4fv(planet.uniforms.color, flatten(data.color));
+  planet.render();
+  ms.pop();
+  
+  
+  
+  name5 = "Venus";
+  planet = Planets[name5];
+  data = SolarSystem[name5];
   
   planet.PointMode = false;
   
