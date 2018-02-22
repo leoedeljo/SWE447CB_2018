6bcea0e70fd57e2ef94951b459e3b8956b43736f
deoelNoveno <leoedeljo@yahoo.com>
2018-02-22 15:29:32 -0800

    Update SolarSystem.js

--- Assignment-5/SolarSystem.js
+++ Assignment-5/SolarSystem.js
@@ -12,13 +12,13 @@ var SolarSystem = {
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
-    radius : 0.949,
+    radius : 5,
     distance : 7.23,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
-    radius : 1,
+    radius : 5,
     distance : 10,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
@@ -30,37 +30,37 @@ var SolarSystem = {
     color : [ 0.5, 0.5, 0.5, 1.0 ]
   },
   Mars : {
-    radius : 0.532,
+    radius : 4,
     distance : 15.2,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
-    radius : 11.21,
+    radius : 9,
     distance : 52.0,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
-    radius : 9.45,
+    radius : 8,
     distance : 95.8,
     year : 29.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
-    radius : 4.01,
+    radius : 7,
     distance : 192.0,
     year : 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
-    radius : 3.88,
+    radius : 7,
     distance : 300.5,
     year : 163.7,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
-    radius : 0.186,
+    radius : 3,
     distance : 394.8,
     year : 247.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
