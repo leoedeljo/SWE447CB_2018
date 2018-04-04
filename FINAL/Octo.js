9b5b6d44182707b22d5038a9df4a1f1f9b9c6b2f
deoelNoveno <leoedeljo@yahoo.com>
2018-04-03 19:50:04 -0700

    Create Octo.js

---
+++ FINAL/Octo.js
@@ -0,0 +1,35 @@
+var octo = undefined;
+var gl = undefined;
+var angle = 0;
+
+function init() {
+  var canvas = document.getElementById( "webgl-canvas" );
+
+  gl = WebGLUtils.setupWebGL( canvas );
+
+  if ( !gl ) {
+    alert("Unable to setup WebGL");
+    return;
+  }
+
+  gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+  gl.enable( gl.DEPTH_TEST );
+
+  octo = new Octohedron();
+
+  render();
+}
+
+function render() {
+  gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+  angle += 2.0; // degrees
+
+  octo.MV = rotate( angle, [1, 1, 0] );
+
+  octo.render();
+
+  requestAnimationFrame( render ); // schedule another call to render()
+}
+
+window.onload = init;
