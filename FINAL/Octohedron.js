660b7264511d4947aface2f0c867cf3185860b66
deoelNoveno <leoedeljo@yahoo.com>
2018-04-03 19:48:22 -0700

    Create Octohedron.js

---
+++ FINAL/Octohedron.js
@@ -0,0 +1,74 @@
+function Octohedron( vertexShaderId, fragmentShaderId ) {
+
+    // Initialize the shader pipeline for this object using either shader ids
+    //   declared in the application's HTML header, or use the default names.
+    //
+    var vertShdr = vertexShaderId || "Cube-vertex-shader";
+    var fragShdr = fragmentShaderId || "Cube-fragment-shader";
+
+    this.program = initShaders(gl, vertShdr, fragShdr);
+
+    if ( this.program < 0 ) {
+        alert( "Error: Octohedron shader pipeline failed to compile.\n\n" +
+            "\tvertex shader id:  \t" + vertShdr + "\n" +
+            "\tfragment shader id:\t" + fragShdr + "\n" );
+        return; 
+    }
+
+    this.positions = { 
+        values : new Float32Array([
+           0, 0, -0.25, // V0
+           0.25, 0, 0, // V1
+           0, 0, 0.25, // V2
+           -0.25, 0, 0, // V3
+           0, 0.5, 0, // V4
+           0, -0.5, 0 // V5
+            ]),
+        numComponents : 3
+    };
+    
+    this.indices = { 
+        values : new Uint16Array([
+            1, 2, 5,
+            5, 2, 3,
+            3, 4, 5,
+            5, 4, 1,
+            1, 4, 6,
+            6, 2, 1,
+            2, 6, 3,
+            3, 6, 4
+        ])
+    };
+    this.indices.count = this.indices.values.length;
+
+    
+    this.positions.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+    gl.bufferData( gl.ARRAY_BUFFER, this.positions.values, gl.STATIC_DRAW );
+
+    this.indices.buffer = gl.createBuffer();
+    gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+    gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, this.indices.values, gl.STATIC_DRAW );
+
+    this.positions.attributeLoc = gl.getAttribLocation( this.program, "vPosition" );
+    gl.enableVertexAttribArray( this.positions.attributeLoc );
+
+    MVLoc = gl.getUniformLocation( this.program, "MV" );
+
+    this.MV = undefined;
+
+    this.render = function () {
+        gl.useProgram( this.program );
+
+        gl.bindBuffer( gl.ARRAY_BUFFER, this.positions.buffer );
+        gl.vertexAttribPointer( this.positions.attributeLoc, this.positions.numComponents,
+            gl.FLOAT, gl.FALSE, 0, 0 );
+ 
+        gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, this.indices.buffer );
+
+        gl.uniformMatrix4fv( MVLoc, gl.FALSE, flatten(this.MV) );
+
+        // Draw the cube's base
+        gl.drawElements( gl.TRIANGLES, this.indices.count, gl.UNSIGNED_SHORT, 0 );
+    }
+};
